9986fb556de089e72fafc6d0e3c363acae6501a9
preciousonah <preciousonah12@gmail.com>
2024-07-10 07:40:41 -0500

    render landing page in parent component to style accordingly

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import React from 'react';
+import './App.css';
+import LandingPage from './Components/LandingPage/LandingPage.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     
-    </>
-  )
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
 }
 
-export default App
+export default App;
